6598f914e33f8714c10983cab3d406df3e622e69
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-10 10:50:04 +0530

    Add method2

--- Kadane's algorithm/method2.cpp
+++ Kadane's algorithm/method2.cpp
@@ -1,3 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 
 class Solution  
 { 
@@ -27,3 +31,27 @@ class Solution
         
     }
 };
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
