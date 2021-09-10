7be9b01c37d5bbb23210465622777aa45319970f
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-10 10:43:49 +0530

    Add method 2

---
+++ Kadane's algorithm/method2.cpp
@@ -0,0 +1,29 @@
+
+class Solution  
+{ 
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long CurrSum=0,maxSum=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            CurrSum+=arr[i];
+            if(CurrSum<0)
+            {
+            CurrSum=0;
+            }
+            maxSum=max(maxSum,CurrSum);
+        }
+        if(maxSum==0)  // If all the elements are negative , maxSum stores the maximum value
+        {
+            sort(arr,arr+n);
+            maxSum=arr[n-1];
+        } 
+        return maxSum;
+        
+    }
+};
